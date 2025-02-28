ad53b9ced1f59df97be322a7cad79700feae724a
Eric Groom <bcgroom@gmail.com>
2025-02-27 22:39:59 -0800

    lazy load lsp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -14,6 +14,8 @@ return {
   {
     -- Main LSP Configuration
     "neovim/nvim-lspconfig",
+    event = { "BufReadPost", "BufNewFile" },
+    cmd = { "LspInfo", "LspInstall", "LspUninstall" },
     dependencies = {
       -- Automatically install LSPs and related tools to stdpath for Neovim
       -- Mason must be loaded before its dependents so we need to set it up here.
