70c401ee0ea875dfc6a7a1fd5575545de566c7f4
Eric Groom <eric@gingerlabs.com>
2025-03-06 11:33:20 -0800

    disable update checker

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -25,7 +25,7 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "base16-material-palenight" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { enabled = false },
   -- no use for rocks, disable error in checkhealth
   rocks = { enabled = false },
 })
