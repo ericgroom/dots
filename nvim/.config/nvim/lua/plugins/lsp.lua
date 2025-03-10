c29fff23ce2bb3fadd764a72efa03d9dd03b54e4
Eric Groom <eric@gingerlabs.com>
2025-03-10 15:00:02 -0700

    fix nixd setup not being called

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -140,6 +140,8 @@ return {
       })
       require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
+      require("lspconfig").nixd.setup({})
+
       require("mason-lspconfig").setup({
         ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
         automatic_installation = false,
