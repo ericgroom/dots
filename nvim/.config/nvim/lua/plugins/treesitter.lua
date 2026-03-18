d90c39e39c9755b20e1ebece924ca60771929373
Eric Groom <ericgroom@macbookair.local>
2026-03-18 12:02:41 -0700

    treesitter update fixes

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,19 +1,22 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local configs = require("nvim-treesitter.configs")
-      configs.setup {
-        ensure_installed = { "lua", "vim", "vimdoc", "typescript", "javascript", "nix", "elixir", "swift", "caddy" },
-        ignore_install = {},
-        sync_install = false,
-        auto_install = false,
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false
-        }
-      }
-    end
-  }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local ensure_installed =
+				{ "lua", "vim", "vimdoc", "typescript", "javascript", "nix", "elixir", "swift", "caddy" }
+			local treesitter = require("nvim-treesitter")
+			treesitter.install(ensure_installed)
+
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function(args)
+					local lang = vim.treesitter.language.get_lang(args.match)
+					-- Only start if a parser and highlight query are actually found
+					if lang and vim.treesitter.query.get(lang, "highlights") then
+						pcall(vim.treesitter.start, args.buf, lang)
+					end
+				end,
+			})
+		end,
+	},
 }
