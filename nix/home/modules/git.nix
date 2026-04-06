ff282a91c757a27cfbca9f59814c2b5958d79467
Eric Groom <bcgroom@gmail.com>
2026-04-06 09:45:36 -0700

    add jjui

--- nix/home/modules/git.nix
+++ nix/home/modules/git.nix
@@ -1,7 +1,10 @@
-inputs:
+{ pkgs, ... }:
 {
   programs.gh.enable = true;
   programs.jujutsu.enable = true;
+  home.packages = [
+    pkgs.jjui
+  ];
   # Start with a simple git configuration (email configured manually per system)
   programs.git = {
     enable = true;
