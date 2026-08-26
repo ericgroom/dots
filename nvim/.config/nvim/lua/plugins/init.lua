07aeff6e113e199af5cc10e344a87019e1f159fc
Eric Groom <bcgroom@gmail.com>
2026-08-26 10:12:16 -0700

    close buffers from telescope

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -25,17 +25,28 @@ return {
 			{ "<leader>o", "<cmd>Telescope live_grep<cr>", desc = "Grep" },
 			{ "<leader>b", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
 		},
-		opts = {
-			extensions = {
-				fzf = {
-					fuzzy = true, -- false will only do exact matching
-					override_generic_sorter = true, -- override the generic sorter
-					override_file_sorter = true, -- override the file sorter
-					case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-					-- the default case_mode is "smart_case"
+		opts = function()
+			local actions = require("telescope.actions")
+			return {
+				pickers = {
+					buffers = {
+						mappings = {
+							i = { ["<C-x>"] = actions.delete_buffer },
+							n = { ["x"] = actions.delete_buffer },
+						},
+					},
 				},
-			},
-		},
+				extensions = {
+					fzf = {
+						fuzzy = true, -- false will only do exact matching
+						override_generic_sorter = true, -- override the generic sorter
+						override_file_sorter = true, -- override the file sorter
+						case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+						-- the default case_mode is "smart_case"
+					},
+				},
+			}
+		end,
 		config = function(plugin, opts)
 			require("telescope").setup(opts)
 			require("telescope").load_extension("fzf")
